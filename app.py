f3b4482baae6430dcb3be3be1d2b66f401a29c40
tadavarthi-rishi <rtadavar@asu.edu>
2024-11-21 16:18:11 -0700

    created a sidebar and welcome message from chatbot

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from langchain_core.messages import AIMessage, HumanMessage
+def get_response(user_input):
+    return "I dont Know"
+
+#app config
+st.set_page_config(page_title="Chat with websites", page_icon=":shark:")
+st.title("Chat with websites")
+chat_history = [
+
+    AIMessage(content = "Hello, I am a chatbot. I can help you with your queries."),
+
+]
+# sidebar
+with st.sidebar:
+    st.header("Settings")
+    website_url = st.text_input("Website Url")
+
+#user input
+user_query = st.chat_input("Type your message here...")
+if user_query is not None and user_query != "":
+
+    response = get_response(user_query)
+    chat_history.append(HumanMessage(content=user_query))
+    chat_history.append(AIMessage(content=response))
+
+
+with st.sidebar:
+    st.write(chat_history)
