a6fc88989047862d834c010ae8b35d26c13247bc
tadavarthi-rishi <rtadavar@asu.edu>
2024-11-21 16:22:07 -0700

    added a session state to preserve the chat history

--- app.py
+++ app.py
@@ -6,11 +6,12 @@ def get_response(user_input):
 #app config
 st.set_page_config(page_title="Chat with websites", page_icon=":shark:")
 st.title("Chat with websites")
-chat_history = [
 
-    AIMessage(content = "Hello, I am a chatbot. I can help you with your queries."),
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = [
+            AIMessage(content = "Hello, I am a chatbot. I can help you with your queries."),
+    ]
 
-]
 # sidebar
 with st.sidebar:
     st.header("Settings")
@@ -21,9 +22,9 @@ user_query = st.chat_input("Type your message here...")
 if user_query is not None and user_query != "":
 
     response = get_response(user_query)
-    chat_history.append(HumanMessage(content=user_query))
-    chat_history.append(AIMessage(content=response))
+    st.session_state.chat_history.append(HumanMessage(content=user_query))
+    st.session_state.chat_history.append(AIMessage(content=response))
 
 
 with st.sidebar:
-    st.write(chat_history)
+    st.write(st.session_state.chat_history)
